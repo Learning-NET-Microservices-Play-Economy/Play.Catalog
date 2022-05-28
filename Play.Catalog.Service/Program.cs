ea89317c3fa2a85ca9b771e4f41d7ade382d080a
Mozart Alkhateeb <mozart.alkhateeb@gmail.com>
2022-05-29 00:12:58 +0300

    fix: cleanup program.cs

--- Play.Catalog.Service/Program.cs
+++ Play.Catalog.Service/Program.cs
@@ -1,6 +1,4 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization;
-using MongoDB.Bson.Serialization.Serializers;
+﻿
 using MongoDB.Driver;
 using Play.Catalog.Service.Entities;
 using Play.Catalog.Service.Repositories;
@@ -17,23 +15,10 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
-        BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-        BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
         serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-        builder.Services.AddSingleton(serviceProvider =>
-        {
-            var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-            var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-            return mongoClient.GetDatabase(serviceSettings.ServiceName);
-        });
-
-        builder.Services.AddSingleton<IRepository<Item>>(serviceProvider =>
-        {
-            var database = serviceProvider.GetService<IMongoDatabase>();
-            return new MongoRepository<Item>(database, "items");
-        });
+        builder.Services.AddMongo();
+        builder.Services.AddMongoRepository<Item>("items");
 
         builder.Services.AddControllers(options =>
         {
