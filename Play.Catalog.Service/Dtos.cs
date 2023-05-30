0bfc40eff824306cba8b224d5906fd7adadf198e
Mozart Alkhateeb <mozart.alkhateeb@gmail.com>
2023-05-31 01:40:18 +0300

    Delete all to start with dotnet 7

--- Play.Catalog.Service/Dtos.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Mozart.Play.Catalog.Service.Dtos
-{
-    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreationDate);
-    public record CreateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
-    public record UpdateItemDto([Required] string Name, string Description, [Range(0, 1000)] decimal Price);
-}
